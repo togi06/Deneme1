c538891d55324a0250c2987966adc96d3818049b
togi06 <t_bayter_06@hotmail.com>
2022-12-11 15:06:07 +0100

    new code

---
+++ src/Deneme1.java
@@ -0,0 +1,13 @@
+public class Deneme1 {
+
+    public static void main(String[] args) {
+
+        int n= 5;
+
+        if (n<5){
+            System.out.println("Hello World");
+        }else{
+            System.out.println("Siktir Git");
+        }
+    }
+}
